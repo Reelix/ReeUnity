f9b697473eeecc03c40a5f6926d5643863c07c6e
Reelix <reelix@gmail.com>
2018-09-09 16:09:51 +0200

    Some initial files

---
+++ Creature.cs
@@ -0,0 +1,172 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using ModelOutline = cakeslice.Outline; // 3D https://assetstore.unity.com/packages/vfx/shaders/fullscreen-camera-effects/outline-effect-78608
+
+public class Creature : MonoBehaviour
+{
+    [Header("Creature Stats")]
+    public string creatureName;
+
+    public int health;
+
+    public int maxHealth;
+
+    public int mana;
+
+    public int maxMana;
+
+    public bool isAlive = true;
+
+    public int damage;
+
+    public float attackRange;
+
+    public NavMeshAgent navAgent;
+
+    [Header("Creature Animations")]
+    public GameObject modelParent; // The parent GameObject containing the bones and the base model
+    public GameObject model; // The base model itself
+    Animation animationController;
+    public AnimationClip moveAnimation;
+    public AnimationClip idleAnimation;
+    public AnimationClip attackAnimation;
+    public AnimationClip deathAnimation;
+
+    ModelOutline modelOutline;
+
+    protected virtual void Start()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+        if (modelParent != null)
+        {
+            if (moveAnimation == null || idleAnimation == null || attackAnimation == null || deathAnimation == null)
+            {
+                Debug.LogWarning("Warning: " + creatureName + " has insufficient animations set to assign an animation controller!");
+            }
+            else
+            {
+                moveAnimation.legacy = true;
+                idleAnimation.legacy = true;
+                attackAnimation.legacy = true;
+                deathAnimation.legacy = true;
+                animationController = modelParent.AddComponent<Animation>();
+                animationController.AddClip(moveAnimation, moveAnimation.name);
+                animationController.AddClip(idleAnimation, idleAnimation.name);
+                animationController.AddClip(attackAnimation, attackAnimation.name);
+                animationController.AddClip(deathAnimation, deathAnimation.name);
+            }
+
+            if (GetComponent<Animator>() != null)
+            {
+                // Whilst I know that Animator is technically newer, the Animation component allows more generic code
+                Debug.LogError("This class doesn't support the Animator - Please drag the animation clips into the appropriate slots and delete the Animator");
+            }
+        }
+
+        if (model == null)
+        {
+            Debug.LogWarning("Please assign the model to this creature script!");
+        }
+        else
+        {
+            modelOutline = model.AddComponent<ModelOutline>();
+            modelOutline.color = 0;
+            modelOutline.enabled = false;
+        }
+    }
+
+    protected virtual void Update()
+    {
+        if (!this.isAlive || animationController == null)
+        {
+            return;
+        }
+
+        if (!this.IsMoving() && !animationController.IsPlaying(idleAnimation.name) && !animationController.IsPlaying(attackAnimation.name))
+        {
+            PlayAnimation(idleAnimation);
+        }
+        else if (this.IsMoving() && !animationController.IsPlaying(moveAnimation.name))
+        {
+            PlayAnimation(moveAnimation);
+        }
+    }
+
+    public void PlayAnimation(string animationName, float animationSpeed = 1f)
+    {
+        if (animationController[animationName] == null)
+        {
+            Debug.LogWarning("The animation " + animationName + " does not exist!");
+            return;
+        }
+        animationController[animationName].speed = animationSpeed;
+        animationController.CrossFade(animationName);
+    }
+
+    public void PlayAnimation(AnimationClip animationClip, float animationSpeed = 1f)
+    {
+        if (animationController[animationClip.name] == null)
+        {
+            Debug.LogWarning("The animation " + animationClip.name + " does not exist as part of the animation controller!");
+            return;
+        }
+        animationController[animationClip.name].speed = animationSpeed;
+        animationController.CrossFade(animationClip.name);
+    }
+
+    public void AlterLife(int lifeValue, Vector3 hitDirection)
+    {
+        health = health + lifeValue;
+        if (health <= 0)
+        {
+            isAlive = false;
+            Die(hitDirection);
+        }
+    }
+
+    protected virtual void Die(Vector3 hitDirection)
+    {
+        Debug.Log("I have died!");
+        animationController.CrossFade(deathAnimation.name);
+    }
+
+    public void DestroyGameObject()
+    {
+        Destroy(gameObject);
+    }
+
+    public void LookAt(Vector3 toLook)
+    {
+        transform.LookAt(new Vector3(toLook.x, 0, toLook.z)); // 0 -> Don't want to tilt up and down :p
+    }
+
+    public void StopMoving()
+    {
+        if (navAgent.isOnNavMesh)
+        {
+            navAgent.isStopped = true;
+        }
+        else
+        {
+            DestroyGameObject();
+        }
+    }
+
+    // Slow things apparently aren't really moving (This is since things slow down before they stop...)
+    public bool IsMoving()
+    {
+        return (navAgent.velocity.magnitude > 1f);
+    }
+
+    public void ToggleOutline()
+    {
+        if (modelOutline != null)
+        {
+            modelOutline.enabled = !modelOutline.enabled;
+        }
+        else
+        {
+            Debug.LogError("You tried to outline a creature that has no outline!");
+        }
+    }
+}
---
+++ ShowFPS.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class ShowFPS : MonoBehaviour
+{
+    GameObject fpsLabel;
+    TMP_Text labelText;
+    float deltaTime = 0.0f;
+    int cooldownTime = 0;
+
+    void Start()
+    {
+        fpsLabel = GameObject.Find("FPS");
+        labelText = fpsLabel.GetComponent<TMP_Text>();
+    }
+
+    void Update()
+    {
+        cooldownTime++;
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        float msec = deltaTime * 1000.0f;
+        float fps = 1.0f / deltaTime;
+        if (cooldownTime % 10 == 0)
+        {
+            cooldownTime = 0;
+            string fpsText = string.Format("{0:0.} fps ({1:0.} ms)", fps, msec);
+            labelText.text = fpsText;
+        }
+    }
+}
