087cfbeceb40795c78fb84314b5476f1c7337af8
Reelix <reelix@gmail.com>
2018-09-12 16:32:26 +0200

    Removed unneeded namespace and usings

--- GizmoDrawer.cs
+++ GizmoDrawer.cs
@@ -1,20 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
 using UnityEngine;
 
-namespace Assets.Scripts
+public class GizmoDrawer
 {
-    public class GizmoDrawer
+    // Thanks to https://github.com/Mckenon
+    public static void DrawCircle(Color color, Transform transform, float radius)
     {
-        // Thanks to https://github.com/Mckenon
-        public static void DrawCircle(Color color, Transform transform, float radius)
-        {
-            Gizmos.color = color;
-            Vector3 point = transform.TransformPoint(Vector3.zero);
-            Matrix4x4 mtx = Gizmos.matrix;
-            Gizmos.matrix = Matrix4x4.TRS(point, Quaternion.identity, new Vector3(1f, 0f, 1f));
-            Gizmos.DrawWireSphere(Vector3.zero, radius);
-            Gizmos.matrix = mtx;
-        }
+        Gizmos.color = color;
+        Vector3 point = transform.TransformPoint(Vector3.zero);
+        Matrix4x4 mtx = Gizmos.matrix;
+        Gizmos.matrix = Matrix4x4.TRS(point, Quaternion.identity, new Vector3(1f, 0f, 1f));
+        Gizmos.DrawWireSphere(Vector3.zero, radius);
+        Gizmos.matrix = mtx;
     }
 }
